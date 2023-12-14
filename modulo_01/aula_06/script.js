e11f58039adb553780d06a2a07d05edb516aa8e1
Anderson Assum <anderson.assum12@gmail.com>
2023-12-14 16:27:57 -0300

    atualização de código

---
+++ modulo_01/aula_06/script.js
@@ -0,0 +1,25 @@
+/*
+let nome = "Anderson";
+
+
+console.log( nome, 'nasceu em 2003')
+console.log( nome, 'em 2019 conheceu Julia')
+console.log( nome, 'ainda não se casou')
+
+*/
+//o método agora é apenas de delcaração
+/*
+let nome; // declarei a variável mas não atribuí valor
+console.log(nome) //mesmo sem atribuir algum valor, a variável recebe undefined. O que é um valor na linguagem Javascript 
+*/
+
+let nome; 
+nome = "Anderson" // Inicializo a variável
+console.log(nome) // Agora a variável responde com a declaração 
+nome = "Assum";
+console.log(nome)
+
+//não podemos criar variáveis com palavras reservadas. Ex: let console/let alert/ let const.
+//variáveis precisam ter nomes significativos.
+//Não podemos redeclarar viariáveis com let 
+//Não utilize var. Utilize LET
