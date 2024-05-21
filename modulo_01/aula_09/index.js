63e3d061d549073b56f26ddc37a5d5d3a3b33e00
Anderson Assum <anderson.assum12@gmail.com>
2024-05-21 16:23:30 -0300

    novas-aulas

---
+++ modulo_01/aula_09/index.js
@@ -0,0 +1,14 @@
+var nome = 'Anderson'
+
+var nome = 'Assum'
+
+console.log(nome)
+
+
+
+
+let jogo = 'Devil May Cry'
+// let jogo = 'Spider-man' - não se pode redeclarar repetindo o let. Mas se for em outro caso...
+jogo = 'Hollow Knight'
+
+console.log(jogo)
---
+++ modulo_01/aula_10/index.js
@@ -0,0 +1,26 @@
+//Dados primitivos são strings, number, undefined e boolean 
+
+//strigns
+
+const nome = 'Anderson';
+const nome1 = "Anderson";
+const nome2 = `Anderson`;
+
+
+//number
+
+const num1 = 10;
+const num2 = 10.52;
+
+//undefined - não aponta pra lugar nenhum na memória
+
+let nomeAluno;
+const sobrenomeAluno = null; //NULO > Há um valor auqi
+
+//boolean - Apenas duas possibilidades de valor.
+
+const aprovado = true
+const reprovado = false
+
+
+console.log("A var" + typeof nome)
