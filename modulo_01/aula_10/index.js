0d801d0a5833d710d411e912b7b7e84bb743f7c3
Anderson Assum <anderson.assum12@gmail.com>
2024-06-03 16:24:35 -0300

    .

--- modulo_01/aula_10/index.js
+++ modulo_01/aula_10/index.js
@@ -17,10 +17,27 @@ const num2 = 10.52;
 let nomeAluno;
 const sobrenomeAluno = null; //NULO > Há um valor auqi
 
-//boolean - Apenas duas possibilidades de valor.
+//boolean - Apenas duas possibilidades de valor. Variável de lógica
 
 const aprovado = true
 const reprovado = false
 
 
-console.log("A var" + typeof nome)
+console.log("A var nome (" + nome + ") é identificada como " + typeof nome)
+
+// const array = [1, 2];
+// const b = array; // esse valor é cópia
+
+// //console.log(array, b)
+
+// b.push(3)
+
+// console.log(array, b)
+
+
+let a = 2
+let b = a
+console.log(a, b) //2 , 2
+
+a = 3;
+console.log(a, b) // 3, 2
---
+++ modulo_01/aula_11/index.js
@@ -0,0 +1,40 @@
+//Aula sobre operadores aritméticos, atribuição e incremento
+
+/*
+- Aritméticos 
+
+. + Adição / Concatenação
+. - Subtração
+. / Divisão
+. * Multiplicação
+. **Potenciação. Elevado a
+. % Resto da divisão
+
+
+
+let num1 = 5
+let num2 = 2
+
+console.log(num1 + num2) //resultado esperado é 7
+console.log(num1 - num2) //resultado esperado é 3
+console.log(num1 * num2) //resultado esperado é 10
+console.log(num1 / num2) //resultado esperado é 2.5
+console.log(num1 ** num2) //resultado esperado é 25
+console.log(num1 % num2) //resultado esperado é 1
+
+console.log('----------')
+let num3 = '5'
+let num4 = '10'
+
+console.log(num3 + num4) //No caso de string, a adição irá fazer a concatenação ao invés da soma. 
+
+console.log('----------')
+
+*/
+
+let contador = 1;
+contador++; //Atribuição de novo valor. O valor da variável + 1
+contador++; //Atribuição de novo valor. O valor da variável + 1
+contador++; //Atribuição de novo valor. O valor da variável + 1
+contador++; //Atribuição de novo valor. O valor da variável + 1
+console.log(contador);
