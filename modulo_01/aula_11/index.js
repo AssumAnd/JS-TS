3cb43d56a6b2975080880b3d2daa7917c8221a4a
Anderson Assum <anderson.assum12@gmail.com>
2024-06-04 16:33:55 -0300

    .

--- modulo_01/aula_11/index.js
+++ modulo_01/aula_11/index.js
@@ -3,12 +3,14 @@
 /*
 - Aritméticos 
 
-. + Adição / Concatenação
-. - Subtração
-. / Divisão
-. * Multiplicação
-. **Potenciação. Elevado a
-. % Resto da divisão
+. + = Adição / Concatenação
+. ++ = Operador de incremento
+. -- = Operador de decremento 
+. - = Subtração
+. / = Divisão
+. * = Multiplicação
+. ** = Potenciação. Elevado a
+. %  = Resto da divisão
 
 
 
@@ -38,3 +40,45 @@ contador++; //Atribuição de novo valor. O valor da variável + 1
 contador++; //Atribuição de novo valor. O valor da variável + 1
 contador++; //Atribuição de novo valor. O valor da variável + 1
 console.log(contador);
+
+let contagem = 10
+contagem--; //Decremento de valor. O valor da variável - 1
+contagem--; //Decremento de valor. O valor da variável - 1
+contagem--; //Decremento de valor. O valor da variável - 1
+contagem--; //Decremento de valor. O valor da variável - 1
+console.log(contagem)
+
+
+
+console.log('----------------')
+
+
+let conta = 2
+//const passo = 50
+
+// conta = conta + passo;
+// console.log(conta);
+// conta = conta + passo;
+// console.log(conta);
+// conta = conta + passo;
+// console.log(conta);
+
+conta *= 30; //A variável multiplicada a 
+
+console.log(conta)
+
+console.log('----------------------')
+
+//Cuidado com os tipos de dados. Tentar somar string com number o resultado é NaN
+
+
+//parseInt é responsável por converter a string para um valor númerico inteiro
+//parseFloat // analisa as casas decimais.
+
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2)
+
+console.log(typeof num2)
+
+
