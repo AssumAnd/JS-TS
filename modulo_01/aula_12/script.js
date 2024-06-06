253eeab02b89dd6ee92f87cb7750bfb9c9e3035a
Anderson Assum <anderson.assum12@gmail.com>
2024-06-06 16:33:50 -0300

    ;

---
+++ modulo_01/aula_12/script.js
@@ -0,0 +1,44 @@
+//window é um código global relacionado a janela do front ent. Por exemplo: window.alert() é a mesma coisa que alert()
+
+//window.confirm('Deseja realmente apagar?') // Retorna um valor booleano entre true e false
+
+//window.prompt('Digite seu nome')
+
+
+// let confirma = confirm('Você gostaria de confirmar?')
+
+// console.log(confirma)
+
+/*
+let num1 = prompt('Digite um número')
+
+console.log(num1)
+
+let num2 = prompt('Digite outro número')
+
+console.log(num2)
+
+
+num1 = Number(num1)
+//Para converter uma string para um number em um prompt, pode se utilizar o Number, parseInt ou parseFloat
+num2 = Number(num2)
+
+//let somaTudo = num1 + num2
+
+alert(`O resultado de soma dos dois números é: ${num1 + num2}`)
+*/
+
+let varA = 'A'; //B
+let varB = 'B';  // C
+let varC = 'C'; //A
+
+// let varT = varA;
+// varA = varB;
+// varB = varC;
+// varC = varT;
+
+
+//[varA, varB, varC] = [varB, varC, varA] -- A mesma coisa dali de cima 
+
+console.log(varA, varB, varC)
+
