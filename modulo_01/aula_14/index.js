6d7b2463dd997a34cb82ac106737b2cab8a97ea9
Anderson Assum <anderson.assum12@gmail.com>
2024-06-19 16:04:09 -0300

    atualização

---
+++ modulo_01/aula_14/index.js
@@ -0,0 +1,47 @@
+// IEEE 754-2008
+
+let num1 = 0.7; // number
+let num2 = 0.1; //number
+let num3 = 3;
+
+
+num1 += num2; //NUM1 = NUM1 + NUM2 //0.8
+num1 += num2;
+num1 += num2;
+
+
+
+num1 = parseFloat(num1.toFixed(2))
+
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+//conversão de NUMBER para string
+//Pode também se fazer a atribuição, redeclarando a variável recebendo ela mesma mais a função.
+//num1 = num1.toString()
+//console.log(typeof num1)
+
+//console.log(num1.toString() + num2)
+
+
+//Arredondar um valor com muitas casas decimais, minha tentativa:
+
+//console.log(parseInt(num1))
+
+//mas também há um método chamado tofixed. Que no parâmetro você define quantas casas decimais pode ir um valor.
+//console.log(num1.toFixed(2))
+
+//Método que retorna se número é um inteiro ou não
+//console.log(Number.isInteger(num3));
+
+
+//Método que retorna se uma variável é um NaN
+// let temp = num1 * 'Olá'
+// console.log(Number.isNaN(temp)); -- true
+//let temp = num1 * '5'
+//console.log(Number.isNaN(temp)); -- true 
+
+
+// let temp = num1 * '5'
+
+// console.log(temp);
---
+++ modulo_01/aula_15/index.js
@@ -0,0 +1,20 @@
+console.log(100 / 0);
+
+
+
+
+// Arredonda pra baixo e cima. Sendo floor pra baixo e ceil pra cima
+// let num2 = Math.floor(num1)
+// let num2 = Math.ceil(num1)
+// let num1 = Math.round(num1)
+
+//Math.max busca em si o maior número de uma sequência . O Min faz o inverso
+// console.log(Math.min(1,2,3,4,5,6,7,8,10,112009, 188, 12093989))
+
+//Esse algorítmo vai me trazer um número aleatório entre 5 e 10. Math random faz a busca e o round inteira ele por completo 
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(aleatorio)
+
+//Código que traz um número elevado a sua raiz quadrada
+//let num1 = 1;
+//console.log(num1 ** 0.5)
